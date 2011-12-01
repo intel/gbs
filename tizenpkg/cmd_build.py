47bb248e78122f2c052fe47c3dfe65c6fc1b85a0
JF Ding <jian-feng.ding@intel.com>
2011-12-01 17:29:57 +0800

    implement 'build' cmd using python

--- tizenpkg/cmd_build.py
+++ tizenpkg/cmd_build.py
@@ -21,7 +21,57 @@
 
 raise ImportError('skip me')
 
+import os
+import tarfile
+
+import git
+import runner
+import msger
+import srcserver as ss
+
 def do(opts, args):
 
-    print opts, args
+    if not os.path.isdir('.git'):
+        msger.error('must run this command under a git tree')
+
+    if git.branch(all=False, current=True)[0] != 'release':
+        msger.error('must run this command under release branch')
+
+    gitsts = git.status()
+    if 'M ' in gitsts or ' M' in gitsts:
+        msger.warning('local changes not committed')
+
+    params = {'parameter': []}
+
+    # pkg:prjname
+    giturl = git.config('remote.origin.url')
+    prjname = os.path.basename(giturl)
+    params['parameter'].append({"name": "pkg",
+                                "value": prjname})
+
+    # obsproject:obsprj
+    params['parameter'].append({"name": "parameters",
+                                "value": "obsproject='%s'" %opts.obsprj})
+
+    # prepare package.tar.bz2
+    tarfp = 'package.tar.bz2'
+    tar = tarfile.open(tarfp, 'w:bz2')
+    for f in git.ls_files():
+        tar.add(f)
+    tar.close()
+
+    params['parameter'].append({"name": tarfp,
+                                "file": "file0"})
+
+    msger.info("Submiting your changes to build server ...")
+    ss.build_trigger(params, tarfp)
+
+    time.sleep(0.5)
+    result = ss.build_mylastresult()
+
+    if result['result'] != 'SUCCESS':
+        msger.error('remote server exception')
+
+    os.remove(tarfp)
+    msger.info('your local changes has been submitted to build server.')
 
