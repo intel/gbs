c7422fb5c1663b09f6e804511def7618d6e5190d
Huaxu Wan <huaxu.wan@intel.com>
2011-12-01 10:16:14 +0800

    queue show to user

--- data/packaging.sh
+++ data/packaging.sh
@@ -205,23 +205,36 @@ get_srctar_md5sum()
     info_msg "Geting md5sum about package $project, at $tag"
     string=`curl -s -i -u$user:$passwd -Fjson='{"parameter": [{"name": "tag", "value": "'$tag'"},{"name":"project", "value":"'$project'"}]}' -FSubmit=Build "$HUDSON_SERVER/job/srctar_md5sum/build"`
     sleep 0.5
+
     last_id=`curl -s -u$user:$passwd "$HUDSON_SERVER/job/srctar_md5sum/lastBuild/buildNumber"`
-
-    # In case the last commit is not made by the user, supposed the last job triggered by '$user' is the one. 
-    while [ ture ]
-    do
-        result_json=`curl -s -u$user:$passwd "$HUDSON_SERVER/job/srctar_md5sum/$last_id/api/json"`
-        username=`echo $result_json|python -mjson.tool |grep "userName" |cut -d'"' -f4`
-        if [ x$username != x$user ]; then
-            last_id=`expr $last_id - 1`
-        else
-            build_id=$last_id
-            break
-        fi
-    done
-
+    result_json=`curl -s -u$user:$passwd "$HUDSON_SERVER/job/srctar_md5sum/$last_id/api/json"`
+    last_prj=`echo $result_json|python -mjson.tool |grep "project" -A1|tail -1|cut -d'"' -f4`
+    last_user=`echo $result_json|python -mjson.tool |grep "userName" |cut -d'"' -f4`
+    # In case the last commit is not made by the user, supposed the last job triggered by '$user' is the one.
+    if [ "$last_prj" != "$project" -o "$last_user" != "$user" ]; then
+        echo "Your request has been put in queue waiting to process"
+        while [ true ]
+        do
+            ret_id=$(curl -s -u$user:$passwd "$HUDSON_SERVER/job/srctar_md5sum/lastBuild/buildNumber") 
+            if [ "$last_id" != "$ret_id" ]; then
+                result_json=`curl -s -u$user:$passwd "$HUDSON_SERVER/job/srctar_md5sum/$ret_id/api/json"`
+                last_prj=`echo $result_json|python -mjson.tool |grep "project" -A1|tail -1|cut -d'"' -f4`
+                last_user=`echo $result_json|python -mjson.tool |grep "userName" |cut -d'"' -f4`
+                if [ "$last_prj" == "$project" -o "$last_user" != "$user" ]; then
+                    build_id=$ret_id
+                    echo ''
+                    break
+                fi
+                last_id=$ret_id
+            else
+                echo -n .
+                sleep 1
+            fi
+        done
+    fi
+    echo 'Processing your request'
     # Waiting until the job finished
-    while [ Ture ]
+    while [ true ]
     do
         result_json=`curl -s -u$user:$passwd "$HUDSON_SERVER/job/srctar_md5sum/$build_id/api/json"`
         status=$(echo $result_json|python -mjson.tool |grep "building.*false")
