6dabe9f73a66b775832db71889182c62f59678dd
Zhang Qiang <qiang.z.zhang@intel.com>
2012-12-10 20:49:45 +0800

    Revert "Use core.meta_get_packagelist() to check the existance of package,#578"
    This reverts commit 774dea087b893e389b40e65d08d4663a1807f6f7.

--- gitbuildsys/oscapi.py
+++ gitbuildsys/oscapi.py
@@ -161,17 +161,13 @@ class OSC(object):
 
         metatype = 'prj'
         path_args = [core.quote_plus(prj)]
+        if pkg:
+            metatype = 'pkg'
+            path_args.append(core.quote_plus(pkg))
         err = None
         try:
             core.meta_exists(metatype = metatype, path_args = tuple(path_args),
                              create_new = False, apiurl = self.apiurl)
-            if not pkg:
-                return True
-
-            if pkg in core.meta_get_packagelist(self.apiurl, prj):
-                return True
-            else:
-                return False
         except urllib2.HTTPError, err:
             if err.code == 404:
                 return False
@@ -183,7 +179,7 @@ class OSC(object):
         if err:
             raise ObsError("can't check if %s/%s exists: %s" % (prj, pkg, err))
 
-        return False
+        return True
 
     def rebuild(self, prj, pkg, arch):
         """Rebuild package."""
